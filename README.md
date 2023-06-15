df8a66318dff27e82358721661375b1d7318b6f7
Toby Kind <44528585+TobySushi@users.noreply.github.com>
2023-06-15 16:44:44 -0400

    initial commit
    Hi

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# gmParallax
- Tiled editor parallax support for gamemaker room .yy exports.
+# gmParallax: Tiled Editor Parallax Support for Gamemaker
+**gmParallax makes importing Tiled Editor parallax behavior seamless by offering an extended export option. Say goodbye to hardcoding parallax for all of your layers!**
+
+![Gif showing many layers and parallax behavior in tiled editor.](/images/exampleA.gif)
+*Work with layers in real time with Tiled*
+![Gif showing the same behavior translated to gamemaker game](/images/exampleB.gif)
+*Quickly export parallax behavior to your gamemaker room!*
---
+++ gmParallax.js
@@ -0,0 +1,56 @@
+// useful functions, thanks eishiya!
+let imageLayers = [];
+
+function getImageLayers(currentLayer) {
+    if(currentLayer.isImageLayer) {
+        imageLayers.push(currentLayer);
+    } else if(currentLayer.isGroupLayer || currentLayer.isTileMap) {
+        var layers = currentLayer.layerCount;
+        for(var layerID = 0; layerID < layers; layerID++) {
+            getImageLayers(currentLayer.layerAt(layerID));
+        }
+    }
+}
+
+// this export generates a room creation code file for gamemaker with the relevant parallax stuff
+tiled.registerMapFormat("gmExtended", {
+    name: "Gamemaker Extended Export",
+    extension: "yy",
+
+    write: (map, filename) => {
+
+        // create .gml file
+        var filePath = FileInfo.path(filename) + '/';
+        var file = new TextFile(filePath + "RoomCreationCode.gml", TextFile.WriteOnly);
+
+        // All we need to do is make the room create a parallax object with the correct values
+        // begin instance creation code
+        file.writeLine("// THIS IS AUTO GENERATED CODE FOR TILED PARALLAX //////////////");
+        file.writeLine("instance_create_depth(0, 0, 0, cont_parallax, {");
+        file.writeLine("    backgroundData: [");  
+
+        // write structs for each background
+        getImageLayers(map);
+        for(var i = 0; i < imageLayers.length; i++) {
+
+            var thisLayer = imageLayers[i];
+
+            // write to file
+            file.writeLine('        { title: "' + thisLayer.name + '", xFactor: ' + thisLayer.parallaxFactor.x + ', yFactor: ' + thisLayer.parallaxFactor.y + ' },');
+
+        }
+
+        // write end of instance creation code
+        file.writeLine("    ]");  
+        file.writeLine("});");  
+        file.writeLine("////////////////////////////////////////////////////"); 
+
+        file.commit();
+
+        // write the actual gamemaker room
+        let format = tiled.mapFormat("yy");
+        format.write(map,filename);
+    
+    }
+
+});
